c80f59dae705a6e2c5f485ed9fea3eb1176c0b36
kasug623 <hacksec421@gmail.com>
2023-02-15 20:57:27 +0900

    update

--- docker_elk_7.10.2/nodejs/src/server.js
+++ docker_elk_7.10.2/nodejs/src/server.js
@@ -7,7 +7,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.redirect(req.baseUrl + '/public/index.html');
+  //res.redirect(req.baseUrl + '/public/index.html');
+  res.redirect(req.baseUrl + '/public/geometry-udemy/index.html');
 });
 
 app.get('/public', (req, res) => {
@@ -15,6 +16,12 @@ app.get('/public', (req, res) => {
   res.redirect(req.baseUrl + '/public/index.html');
 });
 
+app.get('/public/geometry-udemy', (req, res) => {
+  // res.sendFile(__dirname + '/public/index.html');
+  res.redirect(req.baseUrl + '/public/geometry-udemy/index.html');
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/public/geometry-udemy', express.static(__dirname + '/public/geometry-udemy'));
